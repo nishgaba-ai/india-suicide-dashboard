76188bc59ad19ba521cf5759067fe99b0cb0ab9e
Nishchal Gaba <nishgaba.ai@gmail.com>
2025-12-24 09:45:02 +0000

    Updates

---
+++ _next/static/VbvVye63Y_nEN36Oqi3AX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/india-suicide-dashboard//_next/:path+",
+        "destination": "/india-suicide-dashboard/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
